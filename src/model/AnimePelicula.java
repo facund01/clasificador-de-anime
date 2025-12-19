50a8676f9a5bfdb29d6038a66749b13850160557
diego0300 <84690466+diego0300@users.noreply.github.com>
2025-12-19 11:12:18 -0300

    Update AnimePelicula.java

--- src/model/AnimePelicula.java
+++ src/model/AnimePelicula.java
@@ -1,10 +1,13 @@
 package model;
 
 public class AnimePelicula extends Anime {
-    private int duracion;
+    private int duracion; // duración en minutos
 
     public AnimePelicula(String titulo, int anioLanzamiento, String estudio, int duracion) {
         super(titulo, anioLanzamiento, estudio);
+        if (duracion <= 0) {
+            throw new IllegalArgumentException("La duración debe ser mayor a 0");
+        }
         this.duracion = duracion;
     }
 
